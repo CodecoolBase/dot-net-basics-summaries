1891cfa5f54ad305be5e3241905a8b3be76ad4b0
Molnár Attila <attila.molnar@codecool.com>
2017-11-30 11:39:25 +0100

    TimerEvents project added

---
+++ FirstSIWeekSummary/TimerEvents/ProgressForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerEvents
+{
+
+    public partial class ProgressForm : Form
+    {
+        Timer _codecoolTimer;
+
+        public ProgressForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProgressForm_Shown(object sender, EventArgs e)
+        {
+            _codecoolTimer = new Timer
+            {
+                Interval = 1000
+            };
+            _codecoolTimer.Tick += new EventHandler(t_Tick);
+
+            _codecoolTimer.Start();
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 10;
+            if (progressBar1.Value >= 100)
+            {
+                _codecoolTimer.Stop();
+            }
+        }
+    }
+}
