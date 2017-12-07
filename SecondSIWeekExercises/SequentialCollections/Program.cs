8bb1e383aff347750cdcd0d5e8bc4129d9c1b393
Molnár Attila <attila.molnar@codecool.com>
2017-12-07 12:01:42 +0100

    SecondSI week Sequential collectiosn project added

---
+++ SecondSIWeekExercises/SequentialCollections/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace SequentialCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+                Console.WriteLine("From Queue: {0}", obj);
+            }
+
+            Console.WriteLine("------------------------");
+
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From stack: {0}", obj);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
