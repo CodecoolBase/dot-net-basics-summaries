241471cbb590054d02b50cc1b336f94a3ea4d4c3
Molnár Attila <attila.molnar@codecool.com>
2017-12-07 14:14:08 +0100

    SecondSI week Collections summary dmeo codes added

---
+++ SecondSIWeekSummaryDemoCode/CollectionsSummary/CustomGenericClass.cs
@@ -0,0 +1,15 @@
+﻿namespace CollectionsSummary
+{
+    public class CustomGenericClass<T> // generic class
+    {
+        T item;
+        public void Add(T item)
+        {
+            this.item = item;
+        }
+        public override string ToString()
+        {
+            return item.ToString();
+        }
+    }
+}
---
+++ SecondSIWeekSummaryDemoCode/CollectionsSummary/LengthComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsSummary
+{
+    public class LengthComparer : IEqualityComparer<Point>
+    {
+        public bool Equals(Point x, Point y)
+        {
+            return (Math.Abs(x.X) + Math.Abs(x.Y)) == (Math.Abs(y.X) + Math.Abs(y.Y));
+        }
+        public int GetHashCode(Point obj)
+        {
+            return Math.Abs(obj.X) + Math.Abs(obj.Y);
+        }
+    }
+}
